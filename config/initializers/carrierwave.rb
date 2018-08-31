da9a6299f80093b917164041f45478b0037f562e
SINCRARE <SINCRARE@KEIGO-MAC.local>
2018-09-01 05:25:10 +0900

    carrierwaveの設定を更新

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,13 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-  }
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+        region: 'ap-northeast-1'
+    }
 
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'sincrare-diary'
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'sincrare-diary'
+  else
+    config.storage :file
+  end
 end
