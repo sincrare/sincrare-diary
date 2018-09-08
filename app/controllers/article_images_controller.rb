4455483fbc9f37e2d4ceb26ed8948da4bcbd8c14
SINCRARE <SINCRARE@KEIGO-MAC.local>
2018-09-09 08:39:53 +0900

    画像CSV読み込みにforeachを使って改良

--- app/controllers/article_images_controller.rb
+++ app/controllers/article_images_controller.rb
@@ -5,9 +5,7 @@ class ArticleImagesController < ApplicationController
     if article.accesible?(current_user)
       # HerokuのDBレコードを10000件以下（Freeプラン範囲内)に抑えるため、過去の画像データはCSVファイルからパスを読み込む
       if article.id <= 4161
-        article_image_records = CSV.read("config/article_images.csv", headers: true)
-        article_image_records.each do |img|
-          p img
+        CSV.foreach("config/article_images.csv", headers: true) do |img|
           if img["article_id"] == params[:article_id] && img["image_id"] == params[:id]
             s3 = Aws::S3::Client.new(region: ENV['S3_REGION'], access_key_id: ENV['S3_ACCESS_KEY_ID'], secret_access_key: ENV['S3_SECRET_ACCESS_KEY'])
             signer = Aws::S3::Presigner.new(client: s3)
