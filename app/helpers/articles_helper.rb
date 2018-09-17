4fb07a5ef94630d628cc8a3ccdf925a29e9004f3
SINCRARE <SINCRARE@KEIGO-MAC.local>
2018-09-17 13:44:07 +0900

    imgタグの取得 大文字も対応

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   def first_image(content)
-    image_tags = content.match("<img(\s)+.+?>")
+    image_tags = content.match("<(img|IMG)(\s)+.+?>")
     image_tags[0] if image_tags.present?
   end
 end
